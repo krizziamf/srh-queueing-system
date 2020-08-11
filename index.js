22f5088ddd3e5e22bb5e0af39494b06c941146c4
Krizzia Mae Fortich <krizziafortich@gmail.com>
2020-08-11 23:30:02 +0800

    added serve static assets

--- index.js
+++ index.js
@@ -111,10 +111,10 @@ app.post('/emergency', (req, res) => {
 //Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('srh-queueing-system/build'));
+    app.use(express.static('frontend/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../srh-queueing-system/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
     })
 }
 
