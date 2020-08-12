6378315f664b827a21d3a6775b746dfdcaae908d
Krizzia Mae Fortich <krizziafortich@gmail.com>
2020-08-13 00:30:48 +0800

    build folder

---
+++ frontend/build/precache-manifest.32ea76b560c4aec7dc8ea772e56d3f0b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d15f80ab0168da832fe6401dc39eee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fc0ad23e6eba880d1d9",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "11b497b3cbd3e50cdffd",
+    "url": "/static/css/main.50878856.chunk.css"
+  },
+  {
+    "revision": "3fc0ad23e6eba880d1d9",
+    "url": "/static/js/2.d06b126c.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/static/js/2.d06b126c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcdbfb87580484392bb",
+    "url": "/static/js/3.65ead4ce.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.65ead4ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b497b3cbd3e50cdffd",
+    "url": "/static/js/main.495ce41f.chunk.js"
+  },
+  {
+    "revision": "350883635dcd91253f7f",
+    "url": "/static/js/runtime-main.52a15a10.js"
+  },
+  {
+    "revision": "7d0a9df51c6652a266fc39ec9618dd37",
+    "url": "/static/media/dingdong.7d0a9df5.mp3"
+  }
+]);
